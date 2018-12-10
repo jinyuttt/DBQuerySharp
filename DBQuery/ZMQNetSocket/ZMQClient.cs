9477060d1306c47cf1b297280b071c8b97e7f634
jinyuttt <jinyu20072787@sina.com>
2018-12-10 15:38:04 +0800

    增加通信库；ZMQ的TCP通信满足基本要求，但是它没有设置

---
+++ DBQuery/ZMQNetSocket/ZMQClient.cs
@@ -0,0 +1,70 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：ZMQNetSocket
+* 项目描述 ：
+* 类 名 称 ：ZMQClient
+* 类 描 述 ：
+* 命名空间 ：ZMQNetSocket
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：jinyu
+* 创建时间 ：2018
+* 更新时间 ：2018
+* 版 本 号 ：v1.0.0.0
+*******************************************************************
+* Copyright @ jinyu 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+
+
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZeroMQ;
+
+namespace ZMQNetSocket
+{
+    /* ============================================================================== 
+    * 功能描述：ZMQClient 
+    * 创 建 者：jinyu 
+    * 修 改 者：jinyu 
+    * 创建日期：2018 
+    * 修改日期：2018 
+    * ==============================================================================*/
+
+   public class ZMQClient
+    {
+        public void Connect(string host, int port)
+        {
+            using (var client = new RequestSocket(">tcp://localhost:5556"))  // connect
+            {
+
+            }
+        }
+        public void ConnectZMQ(string host, int port)
+        {
+            using (var requester = new ZSocket(ZSocketType.REQ))
+            {
+                // Connect
+                requester.Connect("tcp://127.0.0.1:5555");
+
+                for (int n = 0; n < 10; ++n)
+                {
+                    string requestText = "Hello";
+                    Console.Write("Sending {0}...", requestText);
+
+                    // Send
+                    requester.Send(new ZFrame(requestText));
+
+                    // Receive
+                    using (ZFrame reply = requester.ReceiveFrame())
+                    {
+                        Console.WriteLine(" Received: {0} {1}!", requestText, reply.ReadString());
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ DBQuery/ZMQNetSocket/ZMQServer.cs
@@ -0,0 +1,75 @@
+﻿#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：ZMQNetSocket
+* 项目描述 ：
+* 类 名 称 ：ZMQServer
+* 类 描 述 ：
+* 命名空间 ：ZMQNetSocket
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：jinyu
+* 创建时间 ：2018
+* 更新时间 ：2018
+* 版 本 号 ：v1.0.0.0
+*******************************************************************
+* Copyright @ jinyu 2018. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+
+
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using ZeroMQ;
+
+namespace ZMQNetSocket
+{
+    /* ============================================================================== 
+    * 功能描述：ZMQServer 
+    * 创 建 者：jinyu 
+    * 修 改 者：jinyu 
+    * 创建日期：2018 
+    * 修改日期：2018 
+    * ==============================================================================*/
+
+   public class ZMQServer
+    {
+        public void Start(int port)
+        {
+            using (var responder = new ZSocket(ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://*:"+port);
+
+                while (true)
+                {
+                    // Receive
+                    using (ZFrame request = responder.ReceiveFrame())
+                    {
+                       
+                        // Do some work
+                        Thread.Sleep(1);
+                        // Send
+                        responder.Send(new ZFrame(""));
+                    }
+                }
+            }
+        }
+
+        public void StartRsp(int port)
+        {
+            using (var server = new ResponseSocket("@tcp://localhost:5556"))// bind
+            {
+                byte[] m1 = server.ReceiveFrameBytes();
+                Console.WriteLine("From Client: {0}", m1);
+
+                // Send a response back from the server
+                server.SendFrame("Hi Back");
+
+            }
+        }
+    }
+}
